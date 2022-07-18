101d2f8410dabbe47527b06fcc822b53f0132057
Faizan Rasool <faizanrasool@Faizans-MacBook-Pro.local>
2022-07-18 13:23:32 -0400

    Added to UI

--- client/src/components/CodeEnvironment.tsx
+++ client/src/components/CodeEnvironment.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "./NavBar";
-import DropDownMenu from "./DropDownMenu";
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import { html } from "@codemirror/lang-html";
@@ -8,7 +7,7 @@ import { java } from "@codemirror/lang-java";
 import { cpp } from "@codemirror/lang-cpp";
 import { php } from "@codemirror/lang-php";
 import { python } from "@codemirror/lang-python";
-import { rust } from "@codemirror/lang-rust"
+import { rust } from "@codemirror/lang-rust";
 
 function CodeEnvironment() {
   const [lang, setLang] = React.useState(javascript({ jsx: true }));
@@ -23,14 +22,59 @@ function CodeEnvironment() {
         <div className="h-full md:w-3/5 m-2">
           <CodeMirror
             value=""
-            height="100vh"
+            height="90vh"
             extensions={[lang]}
             onChange={onChange}
             theme={"dark"}
           />
         </div>
         <div className="h-full md:w-2/5 m-2">
-            <DropDownMenu/>
+          <div className="buttons-above-outbput">
+            <label
+              htmlFor="language"
+              className="block mb-2 text-sm font-medium text-[#c7c7c7]"
+            >
+              Select a Language
+            </label>
+            <div className="flex flex-row">
+              <select
+                id="language"
+                className="w-1/3 mr-auto bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 "
+              >
+                <option selected value="js">
+                  JavaScript
+                </option>
+                <option value="java">Java</option>
+                <option value="cpp">C++</option>
+                <option value="php">PHP</option>
+                <option value="python">Python</option>
+                <option value="rust">Rust</option>
+              </select>
+              <button className="w-1/4 m-auto border border-blue-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-blue-700 dark:border-blue-600 dark:placeholder-blue-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                Share
+              </button>
+              <button className="w-1/4 m-auto border border-green-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block p-2.5 dark:bg-green-700 dark:border-green-600 dark:placeholder-green-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500">
+                Run
+              </button>
+            </div>
+          </div>
+
+          <div className="output-area-div mt-5">
+            <label
+              htmlFor="output"
+              className="block mb-2 text-sm font-medium text-[#c7c7c7]"
+            >
+              Output
+            </label>
+            <textarea
+              id="output"
+              style={{ resize: "none" }}
+              disabled
+              rows={15}
+              cols={33}
+              className="my-3 block p-2.5 w-full text-sm text-white rounded-lg bg-[#282c34] border border-[#2c313a]"
+            />
+          </div>
         </div>
       </div>
     </div>
